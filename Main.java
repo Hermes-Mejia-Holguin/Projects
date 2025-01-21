7b4cb1f9fa75f1586ef85bde9d2dfb4e5099d126
Hermes-Mejia-Holguin <mejiahermes98@gmail.com>
2025-01-20 20:15:09 -0500

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hola mundo!");
+    System.out.println("Job title: Software Developer");
+    System.out.println("Why: High growth rate");
+    System.out.println("Median salary:$130,160");
+  }
+}
