566302db6a90abdf26839594193842416ac3008a
Hermes-Mejia-Holguin <mejiahermes98@gmail.com>
2025-01-20 20:50:41 -0500

    changed name

--- First.java
+++ First.java
@@ -1,5 +1,5 @@
 //Hermes Mejia-Holguin
-public class Main {
+public class First {
   public static void main(String[] args) {
     System.out.println("Hola mundo!");
     System.out.println("Job title: Software Developer");
