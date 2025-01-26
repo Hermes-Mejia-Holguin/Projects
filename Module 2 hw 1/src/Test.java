bef87d91282ba189ff648a843e47806a34c0406b
Hermes-Mejia-Holguin <mejiahermes98@gmail.com>
2025-01-26 16:59:46 -0500

    hw done

---
+++ Module 2 hw 1/src/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+    public static void main(String[] args) {
+        double test1= 85.5;
+        double test2= 74.9;
+        double test3= 98.6;
+        double average=(test1+test2+test3)/3;
+
+        System.out.println("Test score 1: "+ test1);
+        System.out.println("Test score 2: " + test2);
+        System.out.println("Test score 3: " + test3);
+        System.out.println("The average of 3 test scores is:" + average);
+        
+    }
+}
